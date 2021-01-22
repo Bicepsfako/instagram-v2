cf50060325bb81cbc4876acb5a720f3e2166a103
Bicepsfako <34413473+Bicepsfako@users.noreply.github.com>
2021-01-23 02:36:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 var Jimp = require('jimp');
 var moment = require('moment');
-moment.locale('tr');
+require('moment/locale/tr');
 
 const USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36';
 (async() => {

89551e867a6de100ac4a44ea529c47c6f7245418
Bicepsfako <34413473+Bicepsfako@users.noreply.github.com>
2021-01-23 06:15:41 +0300

    ⚡ Update

--- index.js
+++ index.js
@@ -20,49 +20,39 @@ const USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, l
 		await page.type('input[name="password"]', process.env.PASSWORD);
 
 		await page.click('button[type="submit"]');
-		blockingWait(4);
+		blockingWait(5);
                 await page.close();
 		const sekme2 = await browser.newPage();
                 await sekme2.setUserAgent(USER_AGENT);
                 await sekme2.goto('https://www.instagram.com/accounts/edit/');
                 const title = await sekme2.title();
-                console.log("Instagram Giriş: " + title);
+                console.log("Trying to login Instagram...");
+                if(title === "Edit Profile • Instagram") {
+                console.log("Successfull!");
+                } else {
+                console.log("Login failed!");
+                sekme2.close();
+                }
 		var inputElement = await sekme2.$('#react-root > section > main > div > article > div > div.LqNQc > div > div > form > input[type="file"]');
 		setInterval(function () {
-
-                        let now = moment().locale('tr');;
-
-			let year = now.get('year');
-
-			let month = now.get('month');
-
-			let date = now.get('date');
-
-			let hour = now.get('hour');
-
-			let minute = now.get('minute');
-
-			let second = now.get('second');
-
-			let milli = now.get('millisecond');
-
-                        var filePath = './resimler/'+minute+'.jpg';
-                        
-			Jimp.read("https://images.wallpapersden.com/image/download/forest-minimal_amxraG2UmZqaraWkpJRoZ2WtaGdl.jpg").then(function (delimg) {
+                        let now = moment();
+                        let minute = now.get('minute');
+                        var filePath = `${minute}.jpg`;
+                        Jimp.read("https://images.wallpapersden.com/image/download/forest-minimal_amxraG2UmZqaraWkpJRoZ2WtaGdl.jpg").then(function (delimg) {
 				Jimp.loadFont(Jimp.FONT_SANS_64_WHITE).then(async function (font) {
 					await delimg.blur(20)
-					await delimg.resize(320, 320)
+					//await delimg.resize(320, 320)
 					await delimg.HORIZONTAL_ALIGN_CENTER;
 					//80 Sağa / //20 Yukarı
 					// 15 sağa / // 80 yukarı
-					await delimg.print(font, 80, 20, hour + ":" + minute, 80)
-					await delimg.print(font, 15, 80, date + "/" + month + "/" + year, 40)
+					await delimg.print(font, 80, 20, moment().format('LT'), 80)
+					await delimg.print(font, 15, 80, moment().format('L'), 40)
 					await delimg.write(`./resimler/${minute}.jpg`);
 					await inputElement.uploadFile(filePath);
                                         if (fs.existsSync(filePath)) {
                                         fs.unlinkSync(filePath);
                                         }
-					console.log("Tarih: " + date + "/" + month + "/" + year + " " + hour + ":" + minute);
+					console.log("Tarih: " + moment().format('LLL'););
 
 				});
 			});
